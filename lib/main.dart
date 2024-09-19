4ae83842ec59f33d1e7d669f88d8d5e43691701d
Murilo Poltronieri Batista De Campos <49900491874@>
2024-09-19 11:00:26 -0300

    construindo a home

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(appBar:AppBar(
         title: const Text("Teste",style: TextStyle(color: Color.fromARGB(255, 82, 223, 0)),),
         backgroundColor: const Color.fromARGB(255, 132, 132, 136),
