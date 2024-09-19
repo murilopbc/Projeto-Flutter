d61a4f55f5ad9ca555315e28318c65f64471f361
Murilo Poltronieri Batista De Campos <49900491874@>
2024-09-19 10:38:09 -0300

    construindo a home

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,49 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(new MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(appBar:AppBar(
         title: const Text("Teste",style: TextStyle(color: Color.fromARGB(255, 82, 223, 0)),),
-        backgroundColor: Color.fromARGB(255, 132, 132, 136),
+        backgroundColor: const Color.fromARGB(255, 132, 132, 136),
         
       ),
             body: Center(child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('Construindo App da Turma', style: TextStyle(fontSize: 24),),SizedBox(height: 20),ElevatedButton(onPressed: () {}, style: ElevatedButton.styleFrom(minimumSize: Size(200, 50),),child: Text('Clique aqui'),)],
-              ), ),     
-      ),
+              children: [Text('Construindo App da Turma', 
+              style: TextStyle(fontSize: 24),),SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {}, 
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 50),
+                ),
+              child: Text('Clique aqui'),
+              )
+              ],
+              ),
+               ), 
+                bottomNavigationBar: const BottomAppBar(
+                  color:  Color.fromARGB(255, 53, 52, 52),
+                  child: Padding(
+                    padding:  EdgeInsets.all(16.0),
+                    child: Text(
+                      'Aqui é o Rodapé',
+                      textAlign: TextAlign.center,
+                    ),
+                     ),
 
+                ),
+
+     
+      ),
+     
     );
   }
 }
